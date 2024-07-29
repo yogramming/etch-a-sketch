b585d47095a71d82aac8eaefc699b54d81c96318
yogramming <yogeshdubey9336@gmail.com>
2024-07-29 22:13:30 +0530

    create main files and common codes in it

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # etch-a-sketch
+
+Check it out: https://yogramming.github.io/etch-a-sketch/
+
+A website which allows the user to draw something on the screen, sort of a etch a sketch but this time digitally. This project is made as a part of an assignment under  an open-source bootcamp called The Odin Project. Usage of flexbox and DOM manipulation are the major thing which is practiced in this project. This is purely meant to showcase this very basic skill that I have recently learnt. Thank you for reading it.
+
+Regards,
+Yogesh Dubey
+(yogramming)
---
+++ script.js
@@ -0,0 +1,28 @@
+const GRIDSIDE = 600;
+let rows = 16;
+let cols = 16;
+
+
+const sketchArea = document.querySelector("#sketch-area")
+sketchArea.style.width = `${GRIDSIDE}px`;
+sketchArea.style.height = `${GRIDSIDE}px`;
+
+function setBackgroundColor() {
+    this.style.backgroundColor = "black";
+}
+function createGridCells() {
+    for (let i = 0; i < (rows * cols); i++) {
+        const gridCell = document.createElement("div");
+        
+        gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        gridCell.classList.add("cell");
+
+        sketchArea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", setBackgroundColor)
+    }  
+}
+
+
+createGridCells();
